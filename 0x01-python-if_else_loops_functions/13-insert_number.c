4137731ae7b4cb8994f55c4122b33dceff2d0443
whizikem <wisdomikem@gmail.com>
2023-01-08 12:15:13 +0000

    Smile in the mirror

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -1,50 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "list.h"
-
+#include "lists.h"
 /**
- * insert_node - insert a new node in a sort list
- * @head: head of linked list to insert
- * @number: number to insert
- * Return: the insert node
+ * insert_node - inserts a node
+ * @head: head
+ * @number: int to add
+ * Return: new node
  */
 listint_t *insert_node(listint_t **head, int number)
 {
-	listint_t *newNode;
-	listint_t *actual;
+    listint_t *temp = NULL, *node = NULL;
 
-	newNode = malloc(sizeof(listint_t));
-	if (newNode == NULL)
-		return (NULL);
-	newNode->n = number;
-	if (*head == NULL)
-	{
-		*head = newNode;
-		newNode->next = NULL;
-		return (newNode);
-	}
-	actual = *head;
-	while (actual)
-	{
-		if (number <= actual->n)
-		{
-			newNode->next = actual;
-			*head = newNode;
-			return (newNode);
-		}
-		if ((number >= actual->n) && (actual->next == NULL))
-		{
-			newNode->next = NULL;
-			actual->next = newNode;
-			return (newNode);
-		}
-		if ((number >= actual->n) && (number <= actual->next->n))
-		{
-			newNode->next = actual->next;
-			actual->next = newNode;
-			return (newNode);
-		}
-		actual = actual->next;
-	}
-	return (NULL);
+    if (head == NULL)
+        return (NULL);
+
+    node = malloc(sizeof(listint_t *));
+    if (node == NULL)
+        return (NULL);
+    node->next = NULL;
+    node->n = number;
+
+    temp = *head;
+    while (temp)
+    {
+        if (temp->n >= number)
+        {
+            node->next = temp;
+            *head = node;
+            return (node);
+        }
+        else if (temp->n <= number && temp->next->n >= number)
+        {
+            if (temp->next != NULL)
+            {
+                node->next = temp->next;
+                temp->next = node;
+                return (node);
+            }
+        }
+        temp = temp->next;
+    }
+
+    return (NULL);
 }

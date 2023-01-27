0b25a860ad53a317061bd4219b6eb329b37b7a49
whizikem <wisdomikem@gmail.com>
2023-01-27 11:30:01 +0000

    tasks 2

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """Documentation for square class"""
 class Square():
-    """A square class."""
-    def __init__(self, size):
-        """Initialization of the square object
-        Args:
-            size (int): the size of the square"""
-        self.__size = size
+	"""A square class."""
+	def __init__(self, size):
+		"""Initialization of the square object
+		Args:
+			size (int): the size of the square"""
+		self.__size = size

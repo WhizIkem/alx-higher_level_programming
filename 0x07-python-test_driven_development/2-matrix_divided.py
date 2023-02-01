936af91a99c273de2161da7c589ac2163c431ef4
whizikem <wisdomikem@gmail.com>
2023-02-01 18:19:35 +0000

    say my name

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -15,25 +15,20 @@ def matrix_divided(matrix, div):
 		A new matrix representing the result of the division
 	"""
 
-mes0 = "matrix must be a matrix (list of lists) of integers/floats"
-mes1 = "Each row of the matrix must have the same size"
-res_matrix = []
+	if (not isinstance(matrix, list) or matrix == [] or
+		not all(isinstance(row, list) for row in matrix) or
+		not all((isinstance(ele, int) or isinstance(ele, float))
+			for ele in [num for row in matrix for num in row])):
+		raise TypeError("matrix must be a matrix (list of lists) of "
+			"integers/floats")
 
-    if not isinstance(div, (int, float)):
-        raise TypeError("div must be a number")
+	if not all(len(row) == len(matrix[0]) for row in matrix):
+		raise TypeError("Each row of the matrix must have the same size")
 
-    if div == 0:
-        raise ZeroDivisionError("division by zero")
+	if not isinstance(div, int) and not isinstance(div, float):
+		raise TypeError("div must be a number")
 
-    for lists in matrix:
-        if len(lists) != len(matrix[0]):
-            raise TypeError(mes1)
-        inner_list = []
-        for items in lists:
-            if not isinstance(items, (int, float)):
-                raise TypeError(mes0)
-            else:
-                inner_list.append(round(items / div, 2))
-        res_matrix.append(inner_list)
+	if div == 0:
+		raise ZeroDivisionError("division by zero")
 
-    return res_matrix
+	return ([list(map(lambda x: round(x / div, 2), row)) for row in matrix])

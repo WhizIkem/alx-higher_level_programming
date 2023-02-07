94541c88b0cc4533c7c13b24f7c867ccdca96230
whizikem <wisdomikem@gmail.com>
2023-02-07 23:51:31 +0000

    all tasks

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -2,8 +2,8 @@
 """This module defines a JSON file-writing function"""
 import json
 
+
 def save_to_json_file(my_obj, filename):
-	"""write an object to a text file using JSON format"""
-	with open(filename, "w", encoding="utf-8") as f:
-	json_str = json.dumps(my_obj)
-	f.write(json_str)
+    """Writes an object to a text file using JSON format"""
+    with open(filename, "w") as f:
+        json.dump(my_obj, f)

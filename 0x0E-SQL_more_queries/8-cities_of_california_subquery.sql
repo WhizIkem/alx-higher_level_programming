414888c7d29f0feb21cc707d08faeecc71519025
whizikem <wisdomikem@gmail.com>
2023-03-26 17:13:25 +0000

    all tasks

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,4 +1,4 @@
-cript that lists all the cities of California that can be found in the database hbtn_0d_usa
+-- Script that lists all the cities of California that can be found in the database hbtn_0d_usa
 SELECT `cities`.`id`, `cities`.`name`
 FROM `cities`, `states`
 WHERE `cities`.`state_id` = `states`.`id`

187540c640591f141eba6051dc14807191b1ecae
whizikem <wisdomikem@gmail.com>
2023-04-24 19:57:38 +0000

    all tasks

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,4 +1,5 @@
 #!/usr/bin/node
-console.log('C is fun');
-console.log('Python is cool'); 
-console.log('JavaScript is amazing');
+const myArray = ['C is fun', 'Python is cool', 'JavaScript is amazing']
+myArray.forEach((element) => {
+  console.log(`${element}`);
+});

dc66175978f45feff329df6dddb28a7a157c5d3f
whizikem <wisdomikem@gmail.com>
2023-06-13 11:16:43 +0000

    Loripsum

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,18 +1,21 @@
 #!/usr/bin/node
 
-const request = require('request');
 const fs = require('fs');
+const request = require('request');
 
 const url = process.argv[2];
 const filePath = process.argv[3];
 
 request(url, (error, response, body) => {
   if (error) {
-    console.log(error);
+    console.error('An error occurred: ${error}');
+  } else if )response.statusCode !== 200) {
+    console.error('Request failed with the status code
+                  ${response.statusCode}');
   } else {
     fs.writeFile(filePath, body, 'utf-8', (error) => {
       if (error) {
-        console.log(error);
+        console.error('An error occurred while saving the file: ${err}');
       } else {
         console.log(`Webpage content has been saved to ${filePath}`);
       }
